0605b6e4840965224d04794f9033c21262c237df
Alexander Averyanov <a.averyanov@themindstudios.com>
2018-05-19 13:53:44 +0300

    user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User
+  include Mongoid::Document
+  include ActiveModel::SecurePassword
+
+  field :email, type: String
+  field :password_digest, type: String
+
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_confirmation, presence: true
+end
